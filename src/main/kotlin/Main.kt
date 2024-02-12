e81a20fa73712970018b7c126684c008057aba19
Musa <79163385+musayuksel@users.noreply.github.com>
2024-02-12 12:22:56 +0000

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() { // this: CoroutineScope
+    println("Main program starts: ${Thread.currentThread().name}")
+    //...
+    println("Main program ends: ${Thread.currentThread().name}")
+}
