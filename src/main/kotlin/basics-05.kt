283a244d0ecc0f731ae35bde7d5176ae0d5eac5e
Musa <79163385+musayuksel@users.noreply.github.com>
2024-02-14 19:31:32 +0000

    Add launch coroutine builder

--- src/main/kotlin/basics-05.kt
+++ src/main/kotlin/basics-05.kt
@@ -1,4 +1,4 @@
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -6,19 +6,19 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking { // Creates a blocking coroutine scope on the main thread
     println("Main program starts: ${Thread.currentThread().name}") // main thread
 
-   launch { // main ????
+    val someDelayedJob: Job = launch { // main -inherit from immediate parent
         doSomeDelayedJobs()
     }
 
-    // Blocks the main thread within runBlocking, waiting for the launched coroutine to complete
-    delay(1500)
+    someDelayedJob.join()//Suspends the coroutine until this job is complete!!!
+
     println("Main program ends: ${Thread.currentThread().name}") // Still main, as runBlocking blocks it
 }
 
-private  suspend fun doSomeDelayedJobs() {
-    println("Coroutine starts: ${Thread.currentThread().name}")
-    waitForTimes(1000) // Calls another suspend function
-    println("Coroutine ends: ${Thread.currentThread().name}")
+private suspend fun doSomeDelayedJobs() {
+    println("Coroutine starts: ${Thread.currentThread().name}")// main
+    waitForTimes(1000) // Calls another suspend function, main thread free(not blocked)
+    println("Coroutine ends: ${Thread.currentThread().name}") // either main or some other thread
 }
 
 private suspend fun waitForTimes(time: Long) {
